dea3ca378071b03e5f0cdef28e1774e38538befd
Mark Jenkins <mark@markjenkins.ca>
2019-04-03 21:09:52 -0500

    defined create_vm using array library for memory and register allocation

---
+++ knightdecode.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+#
+# A derivitive port of:
+# https://github.com/oriansj/stage0/blob/master/vm_decode.c
+#
+# Copyright (C) 2019 Mark Jenkins <mark@markjenkins.ca>
+# This file is part of knightpies
+#
+# knightpies is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# knightpies is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with knightpies.  If not, see <http://www.gnu.org/licenses/>.
+
+from array import array
+
+ARRAY_TYPE_UNSIGNED_CHAR = 'B'
+ARRAY_TYPE_UNSIGNED_SHORT = 'H'
+NUM_REGISTERS = 16
+
+(IP, REG, AMTRAM, MEM, HALTED, EXCEPT, PERF_COUNT) = range(7)
+
+def create_vm(size):
+    instruction_pointer = 0
+    
+    registers = array(ARRAY_TYPE_UNSIGNED_SHORT)
+    for i in xrange(NUM_REGISTERS):
+        registers.append(0)
+
+    amount_of_ram = size
+    
+    memory = array(ARRAY_TYPE_UNSIGNED_CHAR)
+    for i in xrange(size):
+        memory.append(0)
+
+    halted = False
+    exception = False
+    performance_counter = 0
+    
+    return (instruction_pointer, registers, amount_of_ram, memory,
+            halted, exception, performance_counter)
+
+if __name__ == "__main__":
+    vm = create_vm(2**16) # (64*1024)
+    print "vm created %d bytes" %  vm[AMTRAM]
