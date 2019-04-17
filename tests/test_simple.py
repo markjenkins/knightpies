275d1546c2793c760f5047ca5d515a71e731cd21
Mark Jenkins <mark@markjenkins.ca>
2019-04-16 20:37:45 -0500

    another way for testing for all 1 bits on TRUE

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -30,6 +30,7 @@ class SimpleInstructionTests(TestCase):
         # - get us the positive version
         # and we compare that to 2**self.registersize (1 larger than max)
         self.assertEqual( -~self.vm[REG][0], 1<<self.registersize )
+        self.assertEqual( self.vm[REG][0], (1<<self.registersize)-1 )
 
 class SimpleInstructionTests64(SimpleInstructionTests):
     registersize = 64
