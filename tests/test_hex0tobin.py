87a1c4c39eac0ffd2f91cff673b386aefe7ad554
Mark Jenkins <mark@markjenkins.ca>
2019-05-05 23:10:46 -0500

    test stage0_monitor.hex0 encoding itself from hex to binary

--- tests/test_hex0tobin.py
+++ tests/test_hex0tobin.py
@@ -1,12 +1,18 @@
 from unittest import TestCase
 from io import BytesIO
 from hashlib import sha256
+from os import unlink
 
 from stage0dir import get_stage0_file, get_stage0_test_sha256sum
 from hex0tobin import write_binary_filefd_from_hex0_filefd
 from knightdecode import create_vm
-from knightvm_minimal import load_hex_program
+from knightvm_minimal import load_hex_program, grow_memory, execute_vm
 from constants import MEM
+
+from tempfile import mktemp
+
+STACK_START = 0x600
+STACK_SIZE = 8
 
 STAGE_0_MONITOR_HEX_FILEPATH = get_stage0_file('stage0/stage0_monitor.hex0')
 
@@ -30,3 +36,30 @@ class TestHex0ToBin(TestCase):
         vm = create_vm(size=0)
         load_hex_program(vm, STAGE_0_MONITOR_HEX_FILEPATH)
         self.assertEqual( self.stage0_bin_fd.getbuffer(), vm[MEM].tobytes() )
+
+    def test_execute_hex_load(self):
+        output_mem_buffer = BytesIO()
+        input_file_fd = open(
+            get_stage0_file("stage0/stage0_monitor.hex0"), 'rb')
+        tape_01_temp_file_path = mktemp() # deprecated due to security issues
+        tape_02_temp_file_path = mktemp() # deprecated due to security issues
+        vm = create_vm(
+            size=0, registersize=32,
+            tapefile1=tape_01_temp_file_path, tapefile2=tape_02_temp_file_path,
+            stdin=input_file_fd,
+            stdout=output_mem_buffer,
+        )
+        load_hex_program(vm, STAGE_0_MONITOR_HEX_FILEPATH)
+        self.assertEqual( self.stage0_bin_fd.getbuffer(), vm[MEM].tobytes() )
+        grow_memory(vm, STACK_START+STACK_SIZE)
+        execute_vm(vm)
+        input_file_fd.close()
+        tape_file = open(tape_01_temp_file_path, 'rb')
+        checksum = sha256(tape_file.read())
+        tape_file.close()
+        unlink(tape_01_temp_file_path)
+        unlink(tape_02_temp_file_path)
+
+        self.assertEqual(
+            checksum.hexdigest(),
+            get_stage0_test_sha256sum('roms/stage0_monitor') )
