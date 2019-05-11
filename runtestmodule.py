077cbd28cf30acb43076844f9e564be0dcd64fcf
Mark Jenkins <mark@markjenkins.ca>
2019-05-10 21:12:36 -0500

    missing license headers

--- runtestmodule.py
+++ runtestmodule.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
+#
+# Copyright (C) 2019 Mark Jenkins <mark@markjenkins.ca>
+# This file is part of knightpies
+#
+# knightpies is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# knightpies is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with knightpies.  If not, see <http://www.gnu.org/licenses/>.
 
 from sys import argv
 from os.path import split as path_split, join as path_join, splitext, exists
