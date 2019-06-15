90ad8008fd333ca16fb1b58544160e73b785be92
Mark Jenkins <mark@markjenkins.ca>
2019-06-14 19:02:13 -0500

    fixed issue #1, comments can be ended by CR (\r) or LF (\n)

--- hex0tobin.py
+++ hex0tobin.py
@@ -21,39 +21,27 @@ import re
 
 from pythoncompat import write_byte, open_ascii, COMPAT_FALSE, COMPAT_TRUE
 
-def hex_and_whitespace_from_hex0(hex0fd):
-    line = hex0fd.readline()
-    while len(line)>0:
-        # regular expression means # or ; , [0] gets us whatever comes before
-        precomment = re.split('#|;', line)[0]
-        if len(precomment)>0: # there might be no text prior to # or ;
-            yield precomment
-        line = hex0fd.readline()
-
-def int_bytes_from_hex0_whitespace_iter(f):
+def int_bytes_from_hex0_fd(input_file_fd):
     first_nyble = True
     accumulator = 0
-    while True: # until break
-        try:
-            line = next(f)
-        except StopIteration:
-            return # break would have same effect
-        for character in line:
-            if character in hexdigits:
-                accumulator += int(character, 16)
-                if first_nyble:
-                    accumulator = accumulator << 4
-                    first_nyble = False
-                else:
-                    first_nyble = True
-                    yield accumulator
-                    accumulator = 0
-            # else: pass # ignore everything that's not a hexdigit
-
-def int_bytes_from_hex0_fd(input_file_fd):
-    for output_byte in int_bytes_from_hex0_whitespace_iter(
-            hex_and_whitespace_from_hex0(input_file_fd) ):
-        yield output_byte
+    comment = False
+    character = input_file_fd.read(1)
+    while len(character)>0:
+        if not comment and (character==';' or character=='#'):
+            comment = True
+        elif comment and (character=='\r' or character=='\n'):
+            comment = False
+        elif not comment and character in hexdigits:
+            accumulator += int(character, 16)
+            if first_nyble:
+                accumulator = accumulator << 4
+                first_nyble = False
+            else:
+                first_nyble = True
+                yield accumulator
+                accumulator = 0
+        # else: pass # ignore everything that's not a hexdigit
+        character = input_file_fd.read(1)
 
 def write_binary_filefd_from_hex0_filefd(input_file_fd, output_file_fd):
     for output_byte in int_bytes_from_hex0_fd(input_file_fd):
