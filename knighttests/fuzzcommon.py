3c6c29ee1ded321aa50f4e955a078b792f682bff
Mark Jenkins <mark@markjenkins.ca>
2019-07-25 10:51:12 -0500

    common infra for fuzz testing and used to test hex1 encoding

---
+++ knighttests/fuzzcommon.py
@@ -0,0 +1,133 @@
+# Copyright (C) 2019 Mark Jenkins <mark@markjenkins.ca>
+# This file is part of knightpies
+#
+# knightpies is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# knightpies is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with knightpies.  If not, see <http://www.gnu.org/licenses/>.
+
+from random import Random
+from string import hexdigits, printable
+from copy import copy
+from io import StringIO, BytesIO
+
+from hex0tobin import write_binary_filefd_from_hex0_filefd
+from knightdecode import create_vm
+from knightvm_minimal import load_hex_program, grow_memory, execute_vm
+from constants import MEM
+
+from .stage0 import STAGE_0_MONITOR_HEX_FILEPATH
+from .hexcommon import TestHexKnightExecuteCommonSetup, CommonStage1HexEncode
+
+def get_random_for_str(seed_str):
+    r = Random()
+    r.seed(seed_str)
+    return r
+
+GNU_MANIFESTO_RANDOM = get_random_for_str(
+    "What's GNU? Gnu's Not Unix!")
+
+hexdigits_as_ascii_bytes = hexdigits # bytes(hexdigits, encoding='ascii')
+printable_as_ascii_bytes = printable # bytes(printable, encoding='ascii')
+
+# 7 hexdigits for every 3 printable chars
+hex_or_printable = ( (hexdigits_as_ascii_bytes,)*7 +
+                     (printable_as_ascii_bytes,)*3 )
+
+def get_representative_character_byte(random_source, top_level_char_set):
+    char_set = random_source.choice(top_level_char_set)
+    return random_source.choice(char_set)
+
+def get_n_representative_character_bytes(random_source, n, top_level_char_set):
+    return ''.join(
+        get_representative_character_byte(random_source, top_level_char_set)
+        for i in range(n) )
+
+class CommonHexFuzzTest(TestHexKnightExecuteCommonSetup):
+    # implementing sub-classes to provide
+    # get_n_representative_tokens_byte_encode()
+    # encode_input_bytes_w_python_implementation()
+    # get_top_level_char_set()
+
+    random_source_orig = GNU_MANIFESTO_RANDOM
+
+    test_size = 1024*256
+
+    def setUp(self):
+        self.setup_stack_and_tmp_files()
+
+        self.random_source = copy(self.random_source_orig)
+
+        self.input_bytes = StringIO()
+        self.python_output_bytes = BytesIO()
+        self.input_bytes.write(
+            self.get_n_representative_tokens_byte_encoded(self.test_size) )
+        self.input_bytes.seek(0)
+        self.encode_input_bytes_w_python_implementation()
+        self.input_bytes.seek(0)
+
+    def tearDown(self):
+        self.remove_tmp_files()
+
+    def encode_input_bytes_w_python_implementation(self):
+        self.input_encode_python_implementation(
+            self.input_bytes, self.python_output_bytes)
+
+    input_encode_python_implementation = \
+        staticmethod(write_binary_filefd_from_hex0_filefd)
+
+    def get_n_representative_tokens_byte_encoded(self, n):
+        return get_n_representative_character_bytes(
+            self.random_source, n, self.get_top_level_char_set() )
+
+    @staticmethod
+    def get_top_level_char_set():
+        return hex_or_printable
+
+    def write_random_input_to_file(self, filename):
+        with open(filename, 'w') as randominputfile:
+            self.input_bytes.seek(0)
+            for c in self.input_bytes:
+                randominputfile.write(c)
+            self.input_bytes.seek(0)
+
+    def execute_fuzz_test(self):
+        vm = create_vm(
+            size=0, registersize=self.registersize,
+            tapefile1=self.get_tape1_file_path(),
+            tapefile2=self.tape_02_temp_file_path,
+            stdin=self.get_stdin_for_vm(self.input_bytes),
+            stdout=BytesIO(),
+        )
+        self.load_encoding_rom(vm)
+        grow_memory(vm, self.stack_end)
+        execute_vm(vm, optimize=True, halt_print=False)
+
+    def test_output_match(self):
+        self.execute_fuzz_test()
+        with open(self.get_output_file_path(), 'rb') as tape_file:
+            tape_out = tape_file.read()
+            py_out = self.python_output_bytes.getvalue()
+            self.assertEqual(
+                tape_out,
+                py_out,
+            )
+
+    def get_tape1_file_path(self):
+        return self.tape_01_temp_file_path
+
+class CommonStage1Fuzz(CommonHexFuzzTest, CommonStage1HexEncode):
+    def setUp(self):
+        CommonHexFuzzTest.setUp(self)
+        self.write_random_input_to_file(self.tape_01_temp_file_path)
+
+    def get_tape1_file_path(self):
+        return CommonHexFuzzTest.get_tape1_file_path(self)
---
+++ knighttests/test_hex1_fuzz.py
@@ -0,0 +1,95 @@
+# Copyright (C) 2019 Mark Jenkins <mark@markjenkins.ca>
+# This file is part of knightpies
+#
+# knightpies is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# knightpies is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with knightpies.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest import TestCase
+from string import hexdigits, printable, whitespace
+
+from hex0tobin import int_bytes_from_hex0_fd
+from hex1tobin import write_binary_filefd_from_hex1_filefd
+
+from .fuzzcommon import CommonStage1Fuzz
+
+from .stage0 import STAGE_0_HEX1_ASSEMBLER_FILEPATH
+
+printable_less_colon_at = ''.join(
+    c for c in printable
+    if c not in ('@', ':')
+)
+
+DEF, REF = range(2)
+
+printable_not_whitespace = set(printable) - set(whitespace)
+
+# 30 hexdigits for every 12 printable chars for every 1 location define
+# for every 3 reference
+hex_symbols_or_most_printable = ( (hexdigits,)*30 +
+                                  (printable_less_colon_at,)*12,
+                                  (DEF,),
+                                  (REF,)*3,
+)
+
+class Hex1FuzzTest(CommonStage1Fuzz, TestCase):
+    encoding_rom_filename = STAGE_0_HEX1_ASSEMBLER_FILEPATH
+
+    # default 1024*256 runs for 2 hours on my slow machine, *10 only 10 minutes
+    # plus, we're not avoiding the use of symbols too big a relative
+    # offset away so we need to keep this under (2**(16-1) - 1 )*2
+    # because signed 16 bit offsets and every two hex chars is a byte of output
+    test_size = 1024*10
+
+    def setUp(self):
+        CommonStage1Fuzz.setUp(self)
+
+    def tearDown(self):
+        CommonStage1Fuzz.tearDown(self)
+
+    input_encode_python_implementation = \
+        staticmethod(write_binary_filefd_from_hex1_filefd)
+
+    int_bytes_from_rom_encode_file = staticmethod(int_bytes_from_hex0_fd)
+
+    def get_n_representative_tokens_byte_encoded(self, n):
+        self.avail_symbols = list(printable_not_whitespace)
+        self.used_symbols = []
+        self.random_source.shuffle(self.avail_symbols)
+        return self.get_n_representative_character_bytes(n)
+
+    def get_representative_character_byte(self):
+        char_set = self.random_source.choice(hex_symbols_or_most_printable)
+        if char_set[0]==DEF:
+            try:
+                symbol = self.avail_symbols.pop()
+            except IndexError:
+                return '\n'
+            else:
+                self.used_symbols.append(symbol)
+                return '\n:%s\n' % symbol
+        elif char_set[0]==REF:
+            # FIXME, we need to check if these symbols are close
+            # enough for use by maintaining a symbol table and a count
+            # through the final binary
+            # for now, test_size = 1024*10 is quite safe
+            # as a relative offset of (2**(16-1) - 1 )==32767 is
+            # greater than 1024*10/2
+            symbol = self.random_source.choice(self.used_symbols)
+            return '\n@%s\n' % symbol
+        else:
+            return self.random_source.choice(char_set)
+
+    def get_n_representative_character_bytes(self, n):
+        return ''.join(
+            self.get_representative_character_byte()
+            for i in range(n) )
