3761e09a2d8d98a2b1144a6fca9da2445fb0b19b
Mark Jenkins <mark@markjenkins.ca>
2019-07-27 12:15:58 -0500

    stage0_monitor fuzz tested with common fuzz test infrastructure

--- knighttests/test_hex0_fuzz.py
+++ knighttests/test_hex0_fuzz.py
@@ -14,33 +14,20 @@
 # You should have received a copy of the GNU General Public License
 # along with knightpies.  If not, see <http://www.gnu.org/licenses/>.
 
-from random import Random
 from string import hexdigits, printable
-from copy import copy
-from io import StringIO, BytesIO
 from unittest import TestCase
 
 from hex0tobin import (
     write_binary_filefd_from_hex0_filefd,
     int_bytes_from_hex0_fd,
     )
-from knightdecode import create_vm
-from knightvm_minimal import load_hex_program, grow_memory, execute_vm
 
 from .stage0 import (
     STAGE_0_MONITOR_HEX_FILEPATH,
     STAGE_0_HEX1_ASSEMBLER_FILEPATH,
     )
-from .test_hex0tobin import TestHex0KnightExecuteCommon
-from .fuzzcommon import CommonStage1Fuzz
 
-def get_random_for_str(seed_str):
-    r = Random()
-    r.seed(seed_str)
-    return r
-
-GNU_MANIFESTO_RANDOM = get_random_for_str(
-    "What's GNU? Gnu's Not Unix!")
+from .fuzzcommon import CommonHexFuzzTest, CommonStage1Fuzz
 
 hexdigits_as_ascii_bytes = hexdigits # bytes(hexdigits, encoding='ascii')
 printable_as_ascii_bytes = printable # bytes(printable, encoding='ascii')
@@ -63,52 +50,32 @@ def get_n_representative_character_bytes(random_source, n):
     return ''.join( get_representative_character_byte(random_source)
                     for i in range(n) )
 
-class Hex0FuzzTest(TestHex0KnightExecuteCommon):
-    random_source_orig = GNU_MANIFESTO_RANDOM
-    hex0_encoding_rom = STAGE_0_MONITOR_HEX_FILEPATH
-
-    def setUp(self):
-        super().setUp()
-        self.random_source = copy(self.random_source_orig)
-
-        self.input_bytes = StringIO()
-        self.python_output_bytes = BytesIO()
-        self.input_bytes.write(
-            get_n_representative_character_bytes(
-                self.random_source, 1024*256) )
-        self.input_bytes.write( '\n' )
-        self.input_bytes.seek(0)
-        write_binary_filefd_from_hex0_filefd(
-            self.input_bytes, self.python_output_bytes)
-        self.input_bytes.seek(0)
-
-    def execute_fuzz_test(self):
-        vm = create_vm(
-            size=0, registersize=self.registersize,
-            tapefile1=self.get_tape1_file_path(None),
-            tapefile2=self.tape_02_temp_file_path,
-            stdin=self.get_stdin_for_vm(self.input_bytes),
-            stdout=BytesIO(),
-        )
-        load_hex_program(vm, self.hex0_encoding_rom )
-        grow_memory(vm, self.stack_end)
-        execute_vm(vm, optimize=True, halt_print=False)
-
-    def test_output_match(self):
-        self.execute_fuzz_test()
-        with open(self.get_output_file_path(), 'rb') as tape_file:
-            tape_out = tape_file.read()
-            py_out = self.python_output_bytes.getvalue()
-            self.assertEqual(
-                tape_out,
-                py_out,
-            )
-
 class Hex0FuzzCommon:
     input_encode_python_implementation = \
         staticmethod(write_binary_filefd_from_hex0_filefd)
 
     int_bytes_from_rom_encode_file = staticmethod(int_bytes_from_hex0_fd)
+
+class Hex0FuzzTest(Hex0FuzzCommon, CommonHexFuzzTest, TestCase):
+    encoding_rom_filename = STAGE_0_MONITOR_HEX_FILEPATH
+
+    test_size = 1024*256
+
+    def setUp(self):
+        CommonHexFuzzTest.setUp(self)
+
+    def tearDown(self):
+        CommonHexFuzzTest.tearDown(self)
+
+    def get_stdin_for_vm(self, input_file_fd):
+        return input_file_fd
+
+    def get_output_file_path(self):
+        return self.tape_01_temp_file_path
+
+    @staticmethod
+    def get_top_level_char_set():
+        return hex_or_printable
 
 class Hex0FuzzTestAssembler1(CommonStage1Fuzz, Hex0FuzzCommon, TestCase):
     encoding_rom_filename = STAGE_0_HEX1_ASSEMBLER_FILEPATH
