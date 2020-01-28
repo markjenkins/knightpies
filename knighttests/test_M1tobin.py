66cf384ba6522e299a00d67bb924753250222928
Mark Jenkins <mark@markjenkins.ca>
2020-01-27 23:14:24 -0600

    test to assemble to bindary stage0_monitor.s against known sha256sum

--- knighttests/test_M1tobin.py
+++ knighttests/test_M1tobin.py
@@ -14,6 +14,10 @@
 # You should have received a copy of the GNU General Public License
 # along with knightpies.  If not, see <http://www.gnu.org/licenses/>.
 
+from unittest import TestCase
+from io import BytesIO
+from hashlib import sha256
+
 from .stage0 import STAGE_0_HEX2_ASSEMBLER_FILEPATH
 
 from .hexcommon import (
@@ -22,10 +26,23 @@ from .hexcommon import (
 
 from stage0dir import get_stage0_file
 from hex1tobin import write_binary_filefd_from_hex1_filefd
+from M1tobin import M1_files_objs_to_bin
+from pythoncompat import open_ascii
 
 from .test_hex2tobin import (
     get_sha256sum_of_file_after_hex2_encode,
     )
+
+def get_sha256sum_of_file_after_files_are_M1_assembled_and_hex2_linked(
+        *filenames):
+    with BytesIO() as outputmemfile:
+        input_fileobjs = [ open_ascii(filename)
+                           for filename in filenames ]
+        M1_files_objs_to_bin(input_fileobjs, outputmemfile )
+        for f in input_fileobjs:
+            f.close()
+        hexdigest = sha256(outputmemfile.getbuffer()).hexdigest()
+    return hexdigest
 
 class M1Common(HexCommon):
     encoding_rom_filename = STAGE_0_HEX2_ASSEMBLER_FILEPATH
@@ -40,3 +57,17 @@ class Test_M0_256Sum(M1Common, Hex256SumMatch):
     def compute_sha256_digest(self):
         return get_sha256sum_of_file_after_hex2_encode(
             get_stage0_file('stage1/M0-macro.hex2'))
+
+class M1AssembleToBin_Common_256Sum(object):
+    def compute_sha256_digest(self):
+        input_filelist = ['High_level_prototypes/defs'] + self.input_filelist
+        return \
+            get_sha256sum_of_file_after_files_are_M1_assembled_and_hex2_linked(
+                *[get_stage0_file(filename) for filename in input_filelist ]
+            )
+
+
+class Test_M1AssemblerToBin_stage0_monitor(
+        M1AssembleToBin_Common_256Sum, Hex256SumMatch, TestCase):
+    sha256sumfilename = 'roms/stage0_monitor'
+    input_filelist = ['stage0/stage0_monitor.s']
