d8a0082878facfe5813150da65c39302435d2f13
Mark Jenkins <mark@markjenkins.ca>
2020-02-05 23:23:13 -0600

    SR0I explicitly using unsigned immediate

--- knightinstructions.py
+++ knightinstructions.py
@@ -1055,8 +1055,9 @@ def SL0I(vm, c):
     return next_ip
 
 def SR0I(vm, c):
-    mem, register_file, reg0, raw_immediate, next_ip = get_args_for_1OPI(vm, c)
-    register_file[reg0] = register_file[reg0]>>raw_immediate
+    mem, register_file, reg0, unsigned_immediate, next_ip = \
+        get_args_for_1OPI(vm, c, signed_immediate=COMPAT_FALSE)
+    register_file[reg0] = register_file[reg0]>>unsigned_immediate
     return next_ip
 
 def SL1I(vm, c):
